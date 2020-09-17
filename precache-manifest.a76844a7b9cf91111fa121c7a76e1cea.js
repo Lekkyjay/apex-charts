f6e4da3d307c13a3d53bfd627871b231fdfc5cdf
Lekkyjay <lekkyjayjay@yahoo.com>
2020-09-17 22:30:51 +0200

    Updates

---
+++ precache-manifest.a76844a7b9cf91111fa121c7a76e1cea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76924dc051e5f9b85e635646e2157f16",
+    "url": "/apex-charts/index.html"
+  },
+  {
+    "revision": "6d8e1f5e6732505f7295",
+    "url": "/apex-charts/static/css/main.92dd0fd1.chunk.css"
+  },
+  {
+    "revision": "49f903f434fd2c13b204",
+    "url": "/apex-charts/static/js/2.a447fd5d.chunk.js"
+  },
+  {
+    "revision": "4f44a17bb08e78ed1b92485defc539ac",
+    "url": "/apex-charts/static/js/2.a447fd5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d8e1f5e6732505f7295",
+    "url": "/apex-charts/static/js/main.9205d86d.chunk.js"
+  },
+  {
+    "revision": "74323249fa52ab7e68dc",
+    "url": "/apex-charts/static/js/runtime-main.939a9484.js"
+  }
+]);
